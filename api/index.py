21d2c09e7b3664600d25d9bcc0d3c1d9b6d16f1e
dreamfinancee <144824997+dreamfinancee@users.noreply.github.com>
2025-01-16 22:45:03 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,19 +1,22 @@
 import os
 import requests
-from telegram import Update, Bot
+from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, ApplicationBuilder
 
-# Reemplaza 'YOUR TELEGRAM TOKEN HERE' con tu token
+# Configuración del token del bot de telegram
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN', '')
 application = ApplicationBuilder().token(TELEGRAM_API_TOKEN).build()
 
+# Función de inicio
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('¡Hola! Estoy listo para alertarte sobre precios de criptomonedas. Usa /alert nombreactivo valor para configurar una alerta.')
+    update.message.reply_text('¡Hola! Estoy listo para alertarte sobre precios de criptomonedas.')
 
 application.add_handler(CommandHandler('start', start))
 
+# Función principal llamada por Vercel
 def main_handler(event, context):
     return "Bot is running!"
 
 # Inicia la aplicación
 application.run_polling()
+          
